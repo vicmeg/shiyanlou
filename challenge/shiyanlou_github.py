f22963d1eb68a2fb5da7aadedceec45c540741b4
vicmeg <sqyunmu@gmail.com>
2019-05-14 16:39:36 +0800

    spider

---
+++ challenge/shiyanlou_github.py
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+
+import scrapy
+
+class ShiyanlouGithub(scrapy.Spider):
+
+    name = 'shiyanlou-github'
+
+    @property
+    def start_urls(self):
+        return ('https://github.com/shiyanlou?tab=repositories',)
+
+    def parse(self,response):
+        for repository in response.css('li.public'):
+            yield{
+                    'name': repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first(r'\n\s*(.*)'),
+                    'update_time': repository.xpath('.//relative-time/@datetime').extract_first()
+                    }
